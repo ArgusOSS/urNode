8be1185040fc10ffd24b5905cef103b33ca416e6
0x0elliot <wolfwithahat@protonmail.com>
2022-10-25 18:39:43 +0530

    Adding result backend for celery as redis

--- urNode-backend/ur_node/ur_node/settings.py
+++ urNode-backend/ur_node/ur_node/settings.py
@@ -100,6 +100,41 @@ DATABASES = {
 }
 
 
+REDIS_PORT = 6379
+REDIS_DB = 0
+REDIS_HOST = os.environ.get("REDIS_PORT_6379_TCP_ADDR", "redis")
+
+RABBIT_HOSTNAME = os.environ.get("RABBIT_PORT_5672_TCP", "rabbit")
+
+if RABBIT_HOSTNAME.startswith("tcp://"):
+    RABBIT_HOSTNAME = RABBIT_HOSTNAME.split("//")[1]
+
+BROKER_URL = os.environ.get("BROKER_URL", "")
+if not BROKER_URL:
+    BROKER_URL = "amqp://{user}:{password}@{hostname}/{vhost}/".format(
+        user=os.environ.get("RABBIT_ENV_USER", "admin"),
+        password=os.environ.get("RABBIT_ENV_RABBITMQ_PASS", "mypass"),
+        hostname=RABBIT_HOSTNAME,
+        vhost=os.environ.get("RABBIT_ENV_VHOST", ""),
+    )
+
+# We don't want to have dead connections stored on rabbitmq,
+# so we have to negotiate using heartbeats
+BROKER_HEARTBEAT = "?heartbeat=60"
+if not BROKER_URL.endswith(BROKER_HEARTBEAT):
+    BROKER_URL += BROKER_HEARTBEAT
+
+BROKER_POOL_LIMIT = 1
+BROKER_CONNECTION_TIMEOUT = 10
+
+# Set redis as celery result backend
+CELERY_RESULT_BACKEND = "redis://%s:%d/%d" % (REDIS_HOST, REDIS_PORT, REDIS_DB)
+CELERY_REDIS_MAX_CONNECTIONS = 1
+
+# Don't use pickle as serializer, json is much safer
+CELERY_TASK_SERIALIZER = "json"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
